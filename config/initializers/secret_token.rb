626b310b95c0e57aa9666ea88d9ea0299426defd
mayormcmatt <matthew.is.riding@gmail.com>
2014-07-11 16:44:36 -0700

    First commit, let's get this puppy moving

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Globex::Application.config.secret_key_base = '5b448d4999c07f398c118d1a9a938d7fe14415f753f771153c68a7774c88058c4d2f8ae81056682584aef1add143cd4ad5e13cd1db85bcd81c263293b97de9cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Globex::Application.config.session_store :cookie_store, key: '_globex_session'
